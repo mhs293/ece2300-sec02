892c031839ae3ff55bf68ed89804bd6c7b6d21aa
mhs293 <mhs293@cornell.edu>
2024-09-07 15:29:08 -0400

    implemented pair and triple detector in Verilog

--- PairTripleDetector.v
+++ PairTripleDetector.v
@@ -9,6 +9,14 @@ module PairTripleDetector
   input  wire in2,
   output wire out
 );
+  wire w;
+  wire y;
+  wire x;
+
+  or(w, in0, in1);
+  and(x, in0, in1);
+  and(y, w, in2);
+  or(out, y, x);
 
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Discussion Section Task
